d695aceb8f418cb7cf2184474ff5b9f0bff3b9cf
Ed-Games <edmarcosfilho2015@gmail.com>
2023-01-21 17:12:43 -0300

    feat: added view container that renders the weekdays' spans

--- mobile/src/screens/home.tsx
+++ mobile/src/screens/home.tsx
@@ -1,10 +1,24 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
-export function Home(){
-  return(
-    <View className="flex-1 bg-background px-8 pt-16">
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+export function Home() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16 items-center">
       <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, index) => (
+          <Text
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE, height: DAY_SIZE }}
+            key={`${weekDay}-${index}`}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
     </View>
-  )
+  );
 }
